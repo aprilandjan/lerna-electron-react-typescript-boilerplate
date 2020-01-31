69c3d9ae48363c901b1f031f664709f1eac38bdb
merlinye <merlin.ye@qq.com>
2020-01-31 23:42:38 +0800

    chore: adjust babel config

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,16 @@
 /* eslint global-require: off */
 
-const developmentEnvironments = ['development', 'test'];
-
-const developmentPlugins = [
-  require('react-hot-loader/babel')
-];
-
-const productionPlugins = [
-
-  // babel-preset-react-optimize
-  require('@babel/plugin-transform-react-constant-elements'),
-  require('@babel/plugin-transform-react-inline-elements'),
-  require('babel-plugin-transform-react-remove-prop-types')
-];
-
-module.exports = api => {
+module.exports = (api, opts, env) => {
+  const {
+    //  use ts language
+    useTypesScript = true,
+    //  use react related plugins
+    useReact = true,
+  } = opts;
+  const isEnvDevelopment = env === 'development';
+  const isEnvProduction = env === 'production';
+  const isEnvTest = env === 'test';
   // see docs about api at https://babeljs.io/docs/en/config-files#apicache
-
-  const development = api.env(developmentEnvironments);
 
   return {
     presets: [
@@ -27,53 +20,67 @@ module.exports = api => {
           targets: { electron: require('electron/package.json').version }
         }
       ],
-      [require('@babel/preset-react'), { development }]
-    ],
+      useReact && [
+        require('@babel/preset-react'),
+        {
+          development: isEnvDevelopment || isEnvTest,
+          // Will use the native built-in instead of trying to polyfill
+          // behavior for any plugins that require one.
+          useBuiltIns: true,
+        }
+      ],
+      useTypesScript && [
+        require('@babel/preset-typescript'),
+      ],
+    ].filter(Boolean),
     plugins: [
-      // Stage 0
-      //  obj::func(val) => func.call(obj, val)
-      require('@babel/plugin-proposal-function-bind'),
-
-      // Stage 1
-      // FIXME: remove with ts, export v from 'mod-v'
-      require('@babel/plugin-proposal-export-default-from'),
-      // FIXME: remove with ts, a ||= b;
-      require('@babel/plugin-proposal-logical-assignment-operators'),
+      // Turn on legacy decorators for TypeScript files
+      useTypesScript && [
+        require('@babel/plugin-proposal-decorators'),
+        false,
+      ],
+      // class { handleClick = () => { } }
+      // Enable loose mode to use assignment instead of defineProperty
+      // See discussion in https://github.com/facebook/create-react-app/issues/4263
+      [
+        require('@babel/plugin-proposal-class-properties'),
+        {
+          loose: true,
+        },
+      ],
+      //  let budget = 1_000_000_000_000;
+      require('@babel/plugin-proposal-numeric-separator'),
+      require('@babel/plugin-syntax-dynamic-import'),
       //  a?.b
       [require('@babel/plugin-proposal-optional-chaining'), { loose: false }],
-      // FIXME: remove with ts, "hello" |> capitalize |> exclaim
-      [
-        require('@babel/plugin-proposal-pipeline-operator'),
-        { proposal: 'minimal' }
-      ],
       //  var foo = object.foo ?? "default"
       [
         require('@babel/plugin-proposal-nullish-coalescing-operator'),
         { loose: false }
       ],
-      //  FIXME: remove with ts
-      require('@babel/plugin-proposal-do-expressions'),
-
-      // Stage 2
-      [require('@babel/plugin-proposal-decorators'), { legacy: true }],
-      //  FIXME: remove with ts
-      require('@babel/plugin-proposal-function-sent'),
-      //  FIXME: remove with ts, export * as ns from 'mod'
-      require('@babel/plugin-proposal-export-namespace-from'),
-      //  let budget = 1_000_000_000_000;
-      require('@babel/plugin-proposal-numeric-separator'),
-      //  FIXME: remove with ts
-      require('@babel/plugin-proposal-throw-expressions'),
-
-      // Stage 3
-      require('@babel/plugin-syntax-dynamic-import'),
-      // FIXME: ...not needed
-      require('@babel/plugin-syntax-import-meta'),
       [require('@babel/plugin-proposal-class-properties'), { loose: true }],
-      //  FIXME: not important, remove it
-      require('@babel/plugin-proposal-json-strings'),
-
-      ...(development ? developmentPlugins : productionPlugins)
-    ]
+      isEnvDevelopment && [
+        require('react-hot-loader/babel')
+      ],
+      isEnvTest &&
+        // Transform dynamic import to require
+        require('babel-plugin-dynamic-import-node'),
+      ...(isEnvProduction && useReact && [
+        require('@babel/plugin-transform-react-constant-elements'),
+        require('@babel/plugin-transform-react-inline-elements'),
+        require('babel-plugin-transform-react-remove-prop-types')
+      ])
+    ].filter(Boolean),
+    overrides: [
+      useTypesScript && {
+        test: /\.tsx?$/,
+        plugins: [
+          [
+            require('@babel/plugin-proposal-decorators').default,
+            { legacy: true },
+          ],
+        ],
+      },
+    ].filter(Boolean),
   };
 };
