06e5ce44c0650870785c8fa263857209764be644
merlinye <merlin.ye@qq.com>
2020-04-01 17:35:17 +0800

    chore: report cov separately from main & renderer process

--- packages/dev-utils/lib/cov-collect.js
+++ packages/dev-utils/lib/cov-collect.js
@@ -1,11 +1,11 @@
 /** 注入到客户端，上报 cov-report */
-const { ipcMain, ipcRenderer } = require('electron');
+// const { ipcMain, ipcRenderer } = require('electron');
 
 //  一些配置
 const port = process.env.COV_REPORT_PORT || 8889;
 const endPoint = `http://localhost:${port}/coverage/client`;
 const reportInterval = 2000;
-const event = 'COV_REPORT_RENDERER';
+// const event = 'COV_REPORT_RENDERER';
 
 function initReport() {
   if (global.__coverageReportId__) {
@@ -24,22 +24,24 @@ function initReport() {
 
 /** 渲染进程上报 */
 function initReportRenderer(interval) {
-  // const report = () => {
-  //   fetch(endPoint, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(window.__coverage__)
-  //   }).then((data) => {
-  //     console.log(process.type, 'report success');
-  //   }).catch((e) => {
-  //     console.log(process.type, 'report error');
-  //   });
-  // }
   const report = () => {
-    ipcRenderer.send(event, window.__coverage__);
+    fetch(endPoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(window.__coverage__),
+    })
+      .then(data => {
+        // console.log(process.type, 'report success');
+      })
+      .catch(e => {
+        // console.log(process.type, 'report error');
+      });
   };
+  // const report = () => {
+  //   ipcRenderer.send(event, window.__coverage__);
+  // };
   return setInterval(report, interval);
 }
 
@@ -72,9 +74,9 @@ function initReportMain(interval) {
     req.write(data);
     req.end();
   };
-  ipcMain.on(event, (e, rendererCov) => {
-    report(rendererCov);
-  });
+  // ipcMain.on(event, (e, rendererCov) => {
+  //   report(rendererCov);
+  // });
   return setInterval(() => {
     report(global.__coverage__);
   }, interval);
