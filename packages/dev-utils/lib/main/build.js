5079d4675feaf4823d0d1308383d9f995efb2443
merlinye <merlin.ye@qq.com>
2020-08-05 15:23:53 +0800

    chore: remove redundant codes

--- packages/dev-utils/lib/main/build.js
+++ packages/dev-utils/lib/main/build.js
@@ -6,20 +6,9 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
-const fs = require('fs-extra');
-const logger = require('../utils/logger');
-const paths = require('../utils/paths');
 const webpackConfig = require('./webpack.config.prod');
 const webpackBuild = require('../utils/webpackBuild');
 
 webpackBuild(webpackConfig).then(() => {
-  //  copy renderer files
-  if (fs.existsSync(paths.appRendererDist)) {
-    fs.copySync(paths.appRendererDist, paths.appMainDist, {
-      dereference: true,
-    });
-    logger.info(`renderer bundle copied from ${paths.appRendererDist}`);
-  } else {
-    logger.info('renderer bundle not found. Need build renderer first');
-  }
+  //
 });
--- scripts/build.js
+++ scripts/build.js
@@ -9,10 +9,12 @@ const run = require('./utils/run');
   await run('app-main', 'build', false);
 
   //  copy renderer dist into main dist
+  console.log('copy dist assets...');
   const main = findLernaPackages.getSync('app-main').location;
   const mainDist = path.join(main, 'dist');
   const renderer = findLernaPackages.getSync('app-renderer').location;
   const rendererDist = path.join(renderer, 'dist');
   fs.copySync(rendererDist, mainDist);
   fs.copySync(path.join(main, 'index.html'), path.join(mainDist, 'index.html'));
+  console.log('copy dist assets done!');
 })();
