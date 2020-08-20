7f87e5c6070b86b14bb3d7dff2e78bd5ed7fdc74
merlinye <merlin.ye@qq.com>
2020-08-20 14:49:38 +0800

    feat: add router navigation programmatically

--- packages/app-renderer/src/components/Home/index.js
+++ packages/app-renderer/src/components/Home/index.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { push } from 'connected-react-router';
 import routes from '@/constants/routes.json';
 import styles from './style.css';
 import { Button } from 'antd';
+import { dispatch } from '@/store';
 
 /** FIXME: make jest support this webpack alias in require.context */
 const files = require.context('../../assets/img', false, /\.svg$/);
 files.keys().forEach(files);
 
 export default class Home extends Component {
+  handleClick = () => {
+    dispatch(push(routes.COUNTER));
+  };
   render() {
     return (
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+        <Link to={routes.COUNTER}>GO TO Counter PAGE</Link>
         <div>
-          <Button>Button</Button>
+          <Button onClick={this.handleClick}>GO TO COUNTER PAGE</Button>
         </div>
       </div>
     );
